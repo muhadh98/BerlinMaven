ee4a4b9d20727431ba8c1ec0d0fe3615f202fa3d
muhadh98 <mohommedmuhardh2233@gmail.com>
2025-06-04 21:55:38 +0530

    added ameena

--- src/main/java/com/berlin/Main.java
+++ src/main/java/com/berlin/Main.java
@@ -7,5 +7,6 @@ public class Main {
          System.out.println("Hello Muheedh!");
         System.out.println("Hello fazeem!");
         System.out.println("Hello zara!");
+        System.out.println("Hello ammena!");
     }
 }
