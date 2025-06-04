bae8f87aa67423f090e6f052813a2a6b19a88f4e
muhadh98 <mohommedmuhardh2233@gmail.com>
2025-06-04 22:22:54 +0530

    added

--- src/main/java/com/berlin/Main.java
+++ src/main/java/com/berlin/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Muhadh!");
-         System.out.println("Hello Muheedh!");
+        System.out.println("Hello Muheedh!");
         System.out.println("Hello fazeem!");
         System.out.println("Hello zara!");
         System.out.println("Hello ameena!");
+        System.out.println("Hello zara!");
+        System.out.println("Hello zara!");
+        System.out.println("Hello zara!");
+        System.out.println("Hello zara!");
         System.out.println("Hello zara!");
     }
 }
